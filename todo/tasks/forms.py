0c0a996a811435153a8fb355c4ccb8bac04d72b7
dnicholson314 <143367057+dnicholson314@users.noreply.github.com>
2024-03-27 17:43:40 -0400

    Add CSS styles for update task page
    This commit adds styles for the update_task endpoint of the application.
    It also adds a base html template to make adding and styling pages more
    streamlined in the future.

--- todo/tasks/forms.py
+++ todo/tasks/forms.py
@@ -6,3 +6,7 @@ class TaskForm(forms.ModelForm):
     class Meta:
         model = models.Task
         fields = '__all__'
+        widgets = {
+            'title': forms.TextInput(attrs={'placeholder': 'Enter task name'}),
+            'description': forms.Textarea(attrs={'placeholder': 'Enter task description'})
+        }
