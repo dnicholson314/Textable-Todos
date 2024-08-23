5c90c33b8096b1853b14a390c4a7b5f71a3abdfd
dnicholson314 <143367057+dnicholson314@users.noreply.github.com>
2024-08-23 13:07:22 -0400

    Add task form hover

---
+++ static/js/reload.js
@@ -0,0 +1,13 @@
+// Copied from https://stackoverflow.com/questions/17642872/refresh-page-and-keep-scroll-position
+
+document.addEventListener("DOMContentLoaded", function (event) {
+    var scrollpos = sessionStorage.getItem('scrollpos');
+    if (scrollpos) {
+        window.scrollTo(0, scrollpos);
+        sessionStorage.removeItem('scrollpos');
+    }
+});
+
+window.addEventListener("beforeunload", function (e) {
+    sessionStorage.setItem('scrollpos', window.scrollY);
+});
